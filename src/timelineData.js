e87fae5c8427fcc3be8ab0a17da75fb4fc6b2b52
Jay Abella <Jenny.abella1@gmail.com>
2022-02-22 20:04:58 -0800

    Base commit

---
+++ src/timelineData.js
@@ -0,0 +1,16 @@
+let timelineElements = [
+    {
+        id: 1,
+        title: "Title",
+        description: "lorem ipsum dolor sit amet", 
+        date: "January 1, 2022",
+        image: "image"
+    },
+    {
+        id: 2,
+        title: "Title",
+        description: "lorem ipsum dolor sit amet",
+        date: "January 1, 2022",
+        image: "image"
+    }
+];
