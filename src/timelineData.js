b426897581c3c5421c808a4ab6de270d653be3b5
Jay Abella <Jenny.abella1@gmail.com>
2022-03-13 11:50:35 -0700

    Update timeline elements

--- src/timelineData.js
+++ src/timelineData.js
@@ -2,7 +2,7 @@ export const timelineElements = [
     {
         id: 1,
         title: "Roots of the Festival",
-        description: "The roots of CAAMFest can be traced back to Asian CineVision in New York. A year after the film’s release, ACV and NAATA collaborated to form the San Francisco International Asian American Film Festival during this key time. Some films that were featured at the very first SFIAAFF include \"Mirror Points\" by Ruby Yang, \"Chan is Missing\" (1981) by Wayne Wang, \"Manongs\" by Curtis Choy and Chris Chow, and several of Sessue Hayakawa’s films as part of a retrospective on his career. NAATA and ACV would continue to collaborate until 1985.", 
+        description: "The roots of CAAMFest can be traced back to Asian CineVision in New York, as well as the success of \"Chan is Missing\" (1981) by Wayne Wang. A year after the film’s release, ACV and NAATA collaborated to form the San Francisco International Asian American Film Festival during this key time in Asian American film history. Some films that were featured at the very first SFIAAFF include \"Mirror Points\" by Ruby Yang, \"Chan is Missing\", \"Manongs\" by Curtis Choy and Chris Chow, and several of Sessue Hayakawa’s films as part of a retrospective on his career. NAATA and ACV would continue to collaborate until 1985.", 
         date: "1982",
         image: "https://s3.amazonaws.com/caamedia/wp-content/uploads/2015/02/unnamed1-1050x827.jpg",
         imagesrc:"https://caamedia.org/blog/2015/02/25/inaugural-film-festival-1982/",
@@ -11,7 +11,7 @@ export const timelineElements = [
     {
         id: 2,
         title: "A New Journey",
-        description: "After a short break in 1985 to focus on organizing the National Asian American Media Arts Conference at UCLA, NAATA came back to the Bay Area to take full charge of the SFIAAFF. The festival was no longer simply part of ACV, but its own separate entity, marking the start of a new journey for the festival. Due to a scarcity of Asian American films at the time, the festival would often feature films from Asian filmmakers, often from China. From here on, the SFIAAFF continued to grow at a rapid pace, and by the festival’s run in 1994, over 100 works were showcased.",
+        description: "After a short break in 1985 to focus on organizing the National Asian American Media Arts Conference at UCLA, NAATA came back to the Bay Area to take full charge of the SFIAAFF. The festival was no longer simply part of ACV, but its own separate entity, marking the start of a new journey for the festival. Due to a scarcity of Asian American films at the time, the festival would often feature films from Asian filmmakers, typically from China. From here on, the SFIAAFF continued to grow at a rapid pace, and by the festival’s run in 1994, over 100 works were showcased.",
         date: "1986",
         image: "https://bamlive.s3.amazonaws.com/aapi_image-7-aaff-montages-1982-poster-2010-opt_1200.jpg",
         imagesrc:"https://bampfa.org/page/shelves-asian-american-cinema-rarities-bampfa-film-library-study-center",
@@ -20,7 +20,7 @@ export const timelineElements = [
     {
         id: 3,
         title: "Awards and Competition",
-        description: "The 23rd run of SFIAAFF saw an increase in quality Asian American and Canadian works. Festival Director Chi-hui Yang and Assistant Director Taro Goto made the decision to add competitive categories for Best Asian American Feature and Best Asian American Documentary, among other audience awards. CAAMFest thus became a launchpad for Asian American Independent filmmakers. CAAMFest 23 opened with \"Saving Face\" (2004) by Alice Wu and closed with \"The Motel\" (2005) by Michael Kang.",
+        description: "The 23rd run of SFIAAFF saw an increase in quality Asian American and Canadian works. Festival Director Chi-hui Yang and Assistant Director Taro Goto made the decision to add competitive categories for Best Asian American Feature and Best Asian American Documentary, among other audience awards. CAAMFest thus became a launchpad for Asian American Independent filmmakers to kickstart their career. CAAMFest23 opened with \"Saving Face\" (2004) by Alice Wu and closed with \"The Motel\" (2005) by Michael Kang.",
         date: "2005",
         image: "https://s3.amazonaws.com/caamedia/wp-content/uploads/2010/02/ron_ritchie.jpg",
         imagesrc:"https://caamedia.org/blog/2010/02/17/meet-the-jurors/",
@@ -29,7 +29,7 @@ export const timelineElements = [
     {
         id: 4,
         title: "Rebranding to CAAMFest",
-        description: "SFIAAFF originally started out only showcasing Asian American films, but slowly branched out to include films from Asian filmmakers. The festival also began attracting a large number of non-Asian audiences. With the expansion of the festival, NAATA officially rebranded to the Center of Asian American Media. Similarly, SFIAAFF became known as CAAMFest. That year, CAAMFest began including food, music, and digital media alongside its curated showcase of films. ",
+        description: "SFIAAFF originally started out only showcasing Asian American films, but slowly began including films from Asian filmmakers as well. The festival also began attracting a large number of non-Asian audiences. With the expansion of the festival, NAATA officially rebranded to the Center of Asian American Media (CAAM). Similarly, SFIAAFF became known as CAAMFest. That year, CAAMFest began including food, music, and digital media alongside its curated showcase of films. ",
         date: "2013",
         image: "https://s3.amazonaws.com/caamedia/wp-content/uploads/2013/01/CAAMfest.jpg",
         imagesrc:"https://caamedia.org/blog/2013/01/17/introducing-caamfest-2013/",
@@ -38,7 +38,7 @@ export const timelineElements = [
     {
         id: 5,
         title: "The 36th CAAMFest",
-        description: "2018 was the first year that CAAMFest took place in May rather than March, in order to coincide with Asian Pacific American Heritage Month. Around 30,000 patrons attended the festival and viewed 126 works, a far cry from the 13 works that were originally shown at the very first SFIAAFF. At the festival, \"Crazy Rich Asians\" (2018) director Jon M. Chu presented a Mother’s Day event, where he showed never-before-seen clips from his film. Sundance award-winning film \"Minding the Gap\" (2018) by Bing Liu was also screened at CAAMFest 36.",
+        description: "2018 was the first year that CAAMFest took place in May rather than March, in order to coincide with Asian Pacific American Heritage Month. Around 30,000 patrons attended the festival and viewed 126 works, a far cry from the 13 works that were originally shown at the very first SFIAAFF. At the festival, Warner Bros. Pictures collaborated with CAAMFest to present an event in which  \"Crazy Rich Asians\" (2018) director Jon M. Chu showed never-before-seen clips from his film. Sundance award-winning film \"Minding the Gap\" (2018) by Bing Liu was also screened at CAAMFest36.",
         date: "2018",
         image: "https://s3.amazonaws.com/caamedia/wp-content/uploads/2018/02/CF36_SavetheDateWeb2-648x340.jpg",
         imagesrc:"https://caamedia.org/blog/2018/03/21/caamfests-move-to-may-aligns-with-asian-pacific-american-heritage-month-and-caam-public-broadcast-work/",
